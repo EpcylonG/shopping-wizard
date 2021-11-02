639948c5227e73607bf337c292123ee5dae59c2d
Raul Lopez <raul.lopez@singularfactory.net>
2021-11-02 08:43:52 +0000

    fix h2 text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-const current_page = document.querySelector('.pages .page.is-active');
 const sectionPages = document.querySelector(".pages");
 
 const numImages = [5,5,6,6];
@@ -15,7 +14,7 @@ window.onload = () => {
         tab_switcher.addEventListener('click', () => {
             document.querySelector('.nav-list .tab.is-active').classList.remove('is-active');
             tab_switcher.parentNode.classList.add('is-active');
-
+            
             switchPage(page_id);
         });
     }
@@ -26,105 +25,105 @@ function switchPage (page_id) {
     if(page_id != 1) createPage(page_id, numImages[page_id-2]);
 }
 
-function createPage(id, numImages){
+function readElementArray(elementsArray, i, parent){
+    for(let j = 0; j < elementsArray[i].length; j++){
+        parent.appendChild(elementsArray[0]);
+        if(Array.isArray(elementsArray[i][j])){
+            for(let k = 0; k < elementsArray[i][j].length; k++){
+                if(Array.isArray(elementsArray[i][j][k])){
+                    for(let element of elementsArray[i][j][k]){ elementsArray[i][j][k-1].appendChild(element); }
+                } else { elementsArray[i][j-1].appendChild(elementsArray[i][j][k]); }
+            }
+        } else { elementsArray[0].appendChild(elementsArray[i][j]); }
+    }
+}
 
-    const divElement = document.createElement("div");
-    divElement.setAttribute("class", "page is-active");
 
-    const sectionElement = document.createElement("section");
-    sectionElement.setAttribute("id", "all-product-page");
+function createPage(id){
 
-    const sectionTypeImgElement = document.createElement("section");
-    sectionTypeImgElement.setAttribute("class", "type-img");
-    const sectionBigImgElement = document.createElement("section");
-    sectionBigImgElement.setAttribute("class", "big-img");
+    const elements = [elem("div", null, "page is-active"), 
+                     [elem("section", "all-product-page"),
+                        [elem("section", null, "type-img"), 
+                         elem("section", null, "big-img"), 
+                         elem("section", "product-info"),
+                            [elem("h1", "bottle-name"),
+                             elem("pre", "last-p"),
+                             elem("h2", "color-name"),
+                             elem("section", null, "colors-img"),
+                             elem("h2", "size-name"),
+                             elem("div", "prize-button"),
+                                [elem("button", null, "prize-button"),
+                                 elem("button", null, "prize-button"),
+                                 elem("button", null, "prize-button"),
+                                ],
+                             elem("p", null, "product-page"),
+                                [elem("button", "first-buy-botton")]
+                            ]
+                        ]
+                    ]
+                ];
+  
+   for(let i = 0; i < elements.length; i++){
+       if(Array.isArray(elements[i])) {
+           readElementArray(elements[i], i, elements[i-1]);
+        }
+    }
+    sectionPages.appendChild(elements[0]);
+
+    const sectionBigImgElement = document.querySelector(".big-img");
+    const sectionColorsImgElement = document.querySelector(".colors-img");
+    const sectionTypeImgElement = document.querySelector(".type-img");
+
     createImage(folderImages[id-2], id, sectionBigImgElement, sectionTypeImgElement);
-
-    //product-info
-    const sectionProductInfoElement = document.createElement("section");
-    sectionProductInfoElement.setAttribute("id", "product-info");
-
-    const h1Element = document.createElement("h1");
-    h1Element.textContent = folderImages[id-2];
-
-    const preElement = document.createElement("pre");
-    preElement.setAttribute("id", "last-p");
-    preElement.textContent = "Characteristics:\nSingle Layer Stainless Steel Insulation Sports Bottle." + 
-                            "\nStainless steel, without fear of collisions." + 
-                            "\nThick base, vacuum process without noise, maximize the use of space." + 
-                            "\nCarefully polished, burr-free, with the small mouth for easy drinking.";
-
-    preElement.textContent = "Characteristics:\nPortable and durable. Ideal for travel, outdoor activities, car, office, home, school, picnic, hot yoga, hiking, camping, etc." +
-                            "\nMade of 304 stainless steel, vacuum double-layer design, safe, BPA-free and eco-friendly, odorless and easy to clean. It can keep hot or cold water for 12 hours." +
-                            "\nThis vacuum water bottle is also a good container for coffee, juicer, milk, beer, drinks, and daily hydration. The best gift for relatives and friends.";
-
-    preElement.textContent = "Characteristics:\nSuitable for cold water, juice, milk, milk tea, cold drinks, etc." +
-                            "\nEasy to carry, the size fits most car cup holders and bicycle bottle holders." +
-                            "\nA great choice as a gift for friends, classmates and family." +
-                            "\nIt is safe to use, tasteless, beautiful and non-toxic. Provides a relaxed and happy atmosphere.";
-
-    preElement.textContent = "Characteristics:\nStainless steel." +
-                            "\nEco-friendly." +
-                            "\nThermal insulation performance: 6-12 hours.";
-    
-
-
-
-
-    const h2ColorsElement = document.createElement("h2");
-    h2ColorsElement.textContent = "Colors";
-
-    const sectionColorsImgElement = document.createElement("section");
-    sectionColorsImgElement.setAttribute("class", "colors-img");
     createColors(folderImages[id-2], id, sectionColorsImgElement, sectionBigImgElement);
     
-    const h2SizesElement = document.createElement("h2");
-    h2SizesElement.textContent = "Sizes";
+    const h1Element = document.querySelector("#bottle-name");
+    h1Element.textContent = folderImages[id-2];
 
-    const divPrizeElement = document.createElement("div");
-    divPrizeElement.setAttribute("id", "prize-button");
+    const preElement = document.querySelector("#last-p");
+    switch(id) {
+        case "2":
+            preElement.textContent = "Characteristics:\n\nSingle Layer Stainless Steel Insulation Sports Bottle." + 
+                                    "\nStainless steel, without fear of collisions." + 
+                                    "\nThick base, vacuum process without noise, maximize the use of space." + 
+                                    "\nCarefully polished, burr-free, with the small mouth for easy drinking.";
+            break;
+        case "3":
+            preElement.textContent = "Characteristics:\n\nPortable and durable. Ideal for travel, outdoor activities, car, office, home, school, picnic, hot yoga, hiking, camping, etc." +
+                                    "\nMade of 304 stainless steel, vacuum double-layer design, safe, BPA-free and eco-friendly, odorless and easy to clean. It can keep hot or cold water for 12 hours." +
+                                    "\nThis vacuum water bottle is also a good container for coffee, juicer, milk, beer, drinks, and daily hydration. The best gift for relatives and friends.";
+            break;
+        case "4":
+            preElement.textContent = "Characteristics:\n\nSuitable for cold water, juice, milk, milk tea, cold drinks, etc." +
+                                    "\nEasy to carry, the size fits most car cup holders and bicycle bottle holders." +
+                                    "\nA great choice as a gift for friends, classmates and family." +
+                                    "\nIt is safe to use, tasteless, beautiful and non-toxic. Provides a relaxed and happy atmosphere.";
+            break;
+        case "5":
+            preElement.textContent = "Characteristics:\n\nStainless steel." +
+                                    "\nEco-friendly." +
+                                    "\nThermal insulation performance: 6-12 hours.";
+            break;
+    }
+    
 
-    const buttonSizeElement1 = document.createElement("button");
-    buttonSizeElement1.setAttribute("class", "prize-button");
-    buttonSizeElement1.textContent = "330mL";
-    const buttonSizeElement2 = document.createElement("button");
-    buttonSizeElement2.setAttribute("class", "prize-button");
-    buttonSizeElement2.textContent = "500mL";
-    const buttonSizeElement3 = document.createElement("button");
-    buttonSizeElement3.setAttribute("class", "prize-button");
-    buttonSizeElement3.textContent = "1L";
+    const h2Color = document.querySelector("#color-name");
+    h2Color.textContent = "Colors";
+    const h2Size = document.querySelector("#size-name");
+    h2Size.textContent = "Size";
+    const bottleSize = ["330mL", "500mL", "1L"];
+    const buttonSizeElement = document.querySelectorAll(".prize-button");
+    for(let i = 0; i < buttonSizeElement.length; i++) { buttonSizeElement[i].textContent = bottleSize[i]; }
 
-    divPrizeElement.appendChild(buttonSizeElement1);
-    divPrizeElement.appendChild(buttonSizeElement2);
-    divPrizeElement.appendChild(buttonSizeElement3);
-
-    const pPriceElement = document.createElement("p");
-    pElement4.setAttribute("class", "product-page");
-
-    const buttonPriceElement = document.createElement("button");
-    buttonPriceElement.setAttribute("id", "first-buy-botton");
+    const buttonPriceElement = document.querySelector("#first-buy-botton");
     buttonPriceElement.textContent = "Buy";
+}
 
-    pPriceElement.appendChild(buttonPriceElement);
-
-    //product info
-    sectionProductInfoElement.appendChild(h1Element);
-    sectionProductInfoElement.appendChild(preElement);
-    sectionProductInfoElement.appendChild(h2ColorsElement);
-    sectionProductInfoElement.appendChild(sectionColorsImgElement);
-    sectionProductInfoElement.appendChild(h2SizesElement);
-    sectionProductInfoElement.appendChild(divPrizeElement);
-    sectionProductInfoElement.appendChild(pPriceElement);
-    //
-
-    sectionElement.appendChild(sectionTypeImgElement);
-    sectionElement.appendChild(sectionBigImgElement);
-    sectionElement.appendChild(sectionProductInfoElement);
-
-    divElement.appendChild(sectionElement);
-
-    sectionPages.appendChild(divElement);
-
+function elem(type, elemId, elemClass){
+    const element = document.createElement(type);
+    if(elemId != null) element.setAttribute("id", elemId);
+    if(elemClass != null) element.setAttribute("class", elemClass);
+    return element;
 }
 
 function createImage(folder, id, main, lineal){
@@ -167,4 +166,5 @@ function addClickListener(img, main){
     }, false);
 }
 
-//hacer zoom imagen main
+//hacer zoom imagen main 
+//precio
