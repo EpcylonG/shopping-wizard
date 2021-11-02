720935439132905f7ee20e8529ccaab926f79a23
Albert Garcia F <alberd_beto@hotmail.com>
2021-11-02 09:42:52 +0100

    progres barr product

--- js/main.js
+++ js/main.js
@@ -23,3 +23,148 @@ function SwitchPage (page_id) {
     const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
     next_page.classList.add('is-active');
 }
+
+/*BARRA PROGESRO*/
+
+const progreso = document.getElementById('progreso');
+const anterior = document.getElementById('anterior');
+const siguiente = document.getElementById('siguiente');
+const circulos = document.querySelectorAll('.circulo');
+
+let currentActive = 1;
+
+siguiente.addEventListener('click', () => {
+    currentActive++;
+
+    if(currentActive > circulos.length) {
+        currentActive = circulos.length;
+    }
+
+    update();
+
+} );
+
+anterior.addEventListener('click', () => {
+    currentActive--;
+
+    if(currentActive < 1) {
+        currentActive = 1;
+    }
+    
+    update();
+
+} );
+
+function update(){
+    circulos.forEach( (circulo, index) => {
+        if (index < currentActive) {
+            circulo.classList.add('active');   
+        } else {
+            circulo.classList.remove('active');
+        }
+
+    } );
+
+    const actives = document.querySelectorAll('.active');
+
+    progreso.style.width = ((actives.length -1) / (circulos.length -1) )*100 + '%';
+
+    if (currentActive === 1) {
+        anterior.disabled = true;
+    } else if (currentActive === circulos.length) {
+        siguiente.disabled = true;
+    } else {
+        anterior.disabled = false;
+        siguiente.disabled = false;
+    }
+
+}
+
+
+// innputs
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+
+	if(usernameValue === '') {
+		setErrorFor(usernameValue, 'Username cannot be blank');
+	}
+    else {
+		setSuccessFor(username);
+	}
+
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+    
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+
+
+
+
+    const names = document.getElementById('username');
+
+    if (names.value.length <= 5) {
+        setErrorFor(username, 'User name must be longer than 5 characters');
+    }
+    else if (names.value.length >= 20) {
+        setErrorFor(username, 'Username must be less than 20 characters');
+    }
+    else {
+        setSuccessFor(username);
+    }
+
+
+
+
+}
+
+
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
